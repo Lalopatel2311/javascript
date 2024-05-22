26598f89e864040585f8ee6ed592985edb87a158
SahilKumar Sorathiya <130341054+Lalopatel2311@users.noreply.github.com>
2024-05-22 04:08:17 +0000

    done

---
+++ Basic_Program/datatype.js
@@ -0,0 +1,14 @@
+let a = 10
+
+let username = "Sahil"
+
+let age = 12.5
+
+
+let name 
+
+console.log(typeof a)
+console.log(typeof username)
+console.log( typeof age)
+console.log(typeof name)
+console.log(typeof null)
---
+++ Basic_Program/typeconversion.js
@@ -0,0 +1,15 @@
+let name = "Sahil"
+
+let username = Number(name)
+
+console.log(username)
+
+//give the op is Nan= Not a number......... 
+
+let isUserid = 0
+
+let  isuseridconvert = Boolean(isUserid)
+
+console.log(isuseridconvert)
+
+//1==>true and 0==>False  and ""==>False and value "sahil"==>True
