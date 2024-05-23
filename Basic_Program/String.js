a8826c35584f35e0e727df5e7837d38eeab77ce3
SahilKumar Sorathiya <130341054+Lalopatel2311@users.noreply.github.com>
2024-05-23 11:31:16 +0000

    done

---
+++ Basic_Program/String.js
@@ -0,0 +1,18 @@
+let name = "sahilsorathiya"
+let MYname = "SAHIL SORATHIYA"
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(MYname.length)
+
+console.log(`my name is ${MYname}`)
+
+let data = "           Sahil            "
+
+console.log(data)
+console.log(data.trim())
+
+let urldata = "http://localhost?Sahil%20Sorathiya"
+console.log(urldata);
+console.log(urldata.replace("%20","-"));
+
+console.log(urldata.includes("Sahil")) // Give the ans is true or false.........
