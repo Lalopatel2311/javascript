1af83dbbb9fe28dfbde0b52e123e08e694287fed
SahilKumar Sorathiya <130341054+Lalopatel2311@users.noreply.github.com>
2024-05-27 04:56:06 +0000

    done

---
+++ 02_Basic_program/Array.js
@@ -0,0 +1,27 @@
+const mydata = [10,20,30,40,60]
+
+// console.log(mydata)
+// console.log(mydata.length)
+// console.log(mydata[1])
+// console.log(mydata.indexOf(30))
+mydata.push(100)
+console.log(mydata)
+
+mydata.pop()
+console.log(mydata)
+
+// console.log(mydata.unshift(9))
+
+
+//slice
+
+const mydata1 = [1,2,3,4,6]
+// console.log(mydata1)
+// console.log(mydata1.slice(1,2))
+
+//Splice
+
+const myarry = mydata1.splice(1,3)
+// console.log(mydata1)
+// console.log(myarry)
+
