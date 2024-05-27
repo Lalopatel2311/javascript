bbece1cde81a9b12f184b4a21944a1f27596206e
SahilKumar Sorathiya <130341054+Lalopatel2311@users.noreply.github.com>
2024-05-27 04:06:31 +0000

    done

---
+++ Basic_Program/dateshow.js
@@ -0,0 +1,6 @@
+let showdate = new Date()
+
+console.log(showdate);
+console.log(showdate.getDate());
+console.log(showdate.toDateString());
+console.log(showdate.toLocaleDateString());
---
+++ Basic_Program/number_and_math.js
@@ -0,0 +1,15 @@
+console.log(Math.ceil(5.2))
+console.log(Math.ceil(5.9))
+
+console.log(Math.floor(5.2))
+console.log(Math.round(5.2))
+console.log(Math.round(5.8))
+
+console.log(Math.abs(-5.2))
+console.log(Math.sqrt(9))
+
+console.log("Minimum Value:"+ Math.min(20,25,10,5,35))
+console.log("Maximum Value:",Math.max(20,25,10,5,35))
+
+
+
